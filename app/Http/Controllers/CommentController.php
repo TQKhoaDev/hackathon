45a8ff58e1b3bc275e0a088d112695fad1cea411
khoacute <0306221032@caothang.edu.vn>
2024-10-07 15:33:38 +0700

    hello

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    function index (){
+        return View();
+    }
+    
+    function create (){
+        return View();
+    }
+    
+    function delete (){
+        return View();
+    }
+    
+    function update (){
+        return View();
+    }
+}
